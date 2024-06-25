76695f9c951fd06be58d955f63716c128300f47b
tashenoff <kail.dead.kd@gmail.com>
2024-06-25 20:39:02 +0600

    .

--- src/OCRComponent.js
+++ src/OCRComponent.js
@@ -6,17 +6,22 @@ const OCRComponent = () => {
   const [text, setText] = useState('');
   const [image, setImage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [language, setLanguage] = useState('rus');
 
   const handleChange = (event) => {
     setImage(URL.createObjectURL(event.target.files[0]));
     setText(''); // Clear the text when a new image is uploaded
   };
 
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
   const handleClick = () => {
     setIsLoading(true);
     Tesseract.recognize(
       image,
-      'eng',
+      language,
       {
         logger: (m) => console.log(m),
       }
@@ -32,6 +37,10 @@ const OCRComponent = () => {
   return (
     <div className="container">
       <input type="file" onChange={handleChange} />
+      <select onChange={handleLanguageChange} value={language}>
+        <option value="eng">Английский</option>
+        <option value="rus">Русский</option>
+      </select>
       <button onClick={handleClick} disabled={!image || isLoading}>
         Распознать текст
       </button>
